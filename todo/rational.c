1230b07bc87c5355ea814023d88381a24f9ca5f3
Mr. Fleming <efleming@stpatrick.org>
2018-09-11 12:55:11 -0500

    Add files via upload

---
+++ todo/rational.c
@@ -0,0 +1,39 @@
+/*create the strucuture for a rational number
+*  value: type float: the quotient of two ints
+*  numerator: type int: the top
+*  denominator: type int: the bottom
+*/
+
+struct RationalNum {
+    //fill this in
+}
+
+
+//function for adding rational numbers
+struct RationalNum add(struct RationalNum a, struct RationalNum b){
+    //store values in temporary variables
+    //do math
+    //create a new struct
+    //return the new struct
+}
+
+//function for subtracting rational numbers
+struct RationalNum subtract(struct RationalNum a, struct RationalNum b){
+
+}
+
+//function for multiplying rational numbers
+struct RationalNum multiply(struct RationalNum a, struct RationalNum b ){
+
+}
+
+//function for dividing rational numbers
+//cannot divide by zero
+struct RationalNum divide(struct RationalNum a, struct RationalNum b ){
+
+}
+
+//function for adding rational numbers
+struct RationalNum reduce(struct RationalNum a){
+    
+}
